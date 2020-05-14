5cde2e2be22dcc240db23e209113853bf4e395cb
saher sayed abdelaziz <SaherSayed@cis.asu.edu.eg>
2020-05-14 16:54:07 +0200

    Add project files.

---
+++ conncetASPwithTemplate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="conncetASPwithTemplate.MvcApplication" Language="C#" %>
---
+++ conncetASPwithTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(conncetASPwithTemplate.Startup))]
+namespace conncetASPwithTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
