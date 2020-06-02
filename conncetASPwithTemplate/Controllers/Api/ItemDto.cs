e316272abbf479072d65cbe45883981a6245e0db
Saher <saherabdelaziz@yahoo.com>
2020-06-02 17:32:17 +0200

    get the selcted item

---
+++ conncetASPwithTemplate/Controllers/Api/ItemDto.cs
@@ -0,0 +1,7 @@
+﻿namespace conncetASPwithTemplate.Controllers.Api
+{
+    public class ItemDto
+    {
+        public int ItemId { get; set; }
+    }
+}
---
+++ conncetASPwithTemplate/Controllers/Api/ItemsController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using conncetASPwithTemplate.Models;
+
+namespace conncetASPwithTemplate.Controllers.Api
+{
+    public class ItemsController : ApiController
+    {
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+
+        // GET: api/Items
+        public IQueryable<Item> GetItems()
+        {
+            return _context.Items;
+        }
+
+        // GET: api/Items/5
+        public IHttpActionResult GetItem(int id)
+        {
+            Item item = _context.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        // PUT: api/Items/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutItem(int id, Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Items
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult PostItem(Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = item.Id }, item);
+        }
+
+        // DELETE: api/Items/5
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult DeleteItem(int id)
+        {
+            Item item = _context.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+
+            return Ok(item);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ItemExists(int id)
+        {
+            return _context.Items.Count(e => e.Id == id) > 0;
+        }
+    }
+}
